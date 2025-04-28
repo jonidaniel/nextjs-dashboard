dcbb3b8da71d51ca90964fd52e59e2edaf147651
Joni Mäkinen <joni-makinen@live.fi>
2025-04-28 11:26:50 +0300

    Project after Chapter 10.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // experimental: {
+  //   // 'incremental' allows you to adopt PPR for specific routes
+  //   ppr: "incremental",
+  // },
 };
 
 export default nextConfig;
